519547eda26770b5818ae1caed8b7dbe318dbb88
ulrikkenystrup <s215849@dtu.dk>
2023-06-19 21:06:48 +0200

    Ændret filnavne

--- app/src/main/java/com/example/interaktionsprototype3/screens/InteractionScreen.kt
+++ app/src/main/java/com/example/interaktionsprototype3/screens/InteractionScreen.kt
@@ -1,16 +1,8 @@
 package com.example.interaktionsprototype3.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 
