28e5b14c8a65f9f019584e6c4aeb719b75396589
evakathov <evakathov@hotmail.com>
2023-06-20 12:20:04 +0200

    UI Design af InteraktionScreen vol.2

--- app/src/main/java/com/example/interaktionsprototype3/screens/InteractionScreen.kt
+++ app/src/main/java/com/example/interaktionsprototype3/screens/InteractionScreen.kt
@@ -1,13 +1,11 @@
 package com.example.interaktionsprototype3.screens
-
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
@@ -17,18 +15,12 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.interaktionsprototype3.CustomSurface
@@ -95,9 +87,11 @@ fun InteractionScreen() {
         Button(
             onClick = {
             },
-            modifier = Modifier.padding(top = 20.dp)
+            modifier = Modifier.padding(top = 30.dp).width(70.dp).height(50.dp)
+       
         ) {
             Text("Søg")
+           
         }
     }
 }
