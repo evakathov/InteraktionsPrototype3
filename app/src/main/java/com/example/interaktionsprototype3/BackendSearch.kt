df7c2a06e1490b06be570bdc4dedb9eded1f078a
evakathov <evakathov@hotmail.com>
2023-06-21 13:56:00 +0200

    kommentar og lille ændring i DrugScreen

--- app/src/main/java/com/example/interaktionsprototype3/BackendSearch.kt
+++ app/src/main/java/com/example/interaktionsprototype3/BackendSearch.kt
@@ -1,5 +1,6 @@
 package com.example.interaktionsprototype3
 
+import androidx.compose.ui.text.toLowerCase
 import org.xmlpull.v1.XmlPullParser
 import org.xmlpull.v1.XmlPullParserFactory
 import java.io.InputStream
@@ -16,16 +17,20 @@ fun getIDStofByMedicationName(xmlInputStream: InputStream, medicationName: Strin
     while (eventType != XmlPullParser.END_DOCUMENT) {
         when (eventType) {
             XmlPullParser.START_TAG -> {
+                // søger efter startags der hedder Navn
                 val tagName = parser.name
                 if (tagName == "Navn") {
+                    // tildeler variablen currentMedication værdien af indholdet i Navn tagget med parser.nextText()
                     currentMedication = parser.nextText()
                 } else if (tagName == "ID_Stof") {
+                    //tildeler variablen currentIDStof værdien af indholdet i ID_Stof tagget
                     currentIDStof = parser.nextText()
                 }
             }
             XmlPullParser.END_TAG -> {
                 val tagName = parser.name
-                if (tagName == "STOF" && currentMedication == medicationName) {
+                //Hvis end tagget er STOF og currentMedication er det samme som det brugerindtastede præparat returner tilsvarende ID_Stof
+                if (tagName == "STOF" && currentMedication == medicationName.toLowerCase()) {
                     return currentIDStof
                 }
             }
